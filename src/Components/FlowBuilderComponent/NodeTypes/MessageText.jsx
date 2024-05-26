2eb87f93c7efe2dcf004f8a6e68b1d25e2f785f9
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-26 15:26:37 +0530

    custom node update

--- src/Components/FlowBuilderComponent/NodeTypes/MessageText.jsx
+++ src/Components/FlowBuilderComponent/NodeTypes/MessageText.jsx
@@ -1,16 +1,53 @@
 import React from "react";
 
+import MessageIcon from "../../../assets/Icons/MessageIcon.png";
+import WhatsappIcon from "../../../assets/Icons/WhatsappIcon.png";
+
 const MessageText = ({ data: { amount } }) => {
   return (
     <div
       style={{
         width: "120px",
         height: "40px",
-        backgroundColor: "red",
         borderRadius: "5px",
+        boxShadow: "0px 0px 7px rgba(0, 0, 0, 0.3)",
       }}
     >
-      <div></div>
+      {/* node head section */}
+      <div
+        style={{
+          width: "100%",
+          height: "15px",
+          backgroundColor: "#B2F0E3",
+          borderTopLeftRadius: "5px",
+          borderTopRightRadius: "5px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {/* message icon and send message text */}
+        <div
+          style={{ display: "flex", alignItems: "center", marginLeft: "5px" }}
+        >
+          <img src={MessageIcon} alt="Message Icon" width="8" height="8" />
+
+          <p style={{ fontSize: "7px", marginLeft: "5px" }}>Send Message</p>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "50%",
+            padding: "1px",
+            backgroundColor: "#fff",
+            marginRight: "5px",
+          }}
+        >
+          <img src={WhatsappIcon} alt="Message Icon" width="8" height="8" />
+        </div>
+      </div>
     </div>
   );
 };
