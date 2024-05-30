eddee71d08acf57da30b83bc695c9e687a04e7df
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-30 21:48:22 +0530

    code clean-up

--- src/Components/SettingsPanelComponent/MessageText.jsx
+++ src/Components/SettingsPanelComponent/MessageText.jsx
@@ -34,37 +34,38 @@ const MessageText = ({ backBtnHandle }) => {
 
       {/* text area */}
       <div style={{ borderBottom: "1px solid #D9D9D9" }}>
-      <div style={{ borderBottom: "1px solid #D9D9D9" }}>
-        <p
-          style={{
-            color: "#B7B7B7",
-            fontSize: 18,
-            margin: "30px 0 0 10px",
-          }}
-        >
-          Text
-        </p>
+        <div style={{ borderBottom: "1px solid #D9D9D9" }}>
+          <p
+            style={{
+              color: "#B7B7B7",
+              fontSize: 18,
+              margin: "30px 0 0 10px",
+            }}
+          >
+            Text
+          </p>
 
-        <textarea
-          placeholder="Enter your message..."
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          style={{
-            fontSize: 16,
-            margin: "20px 10px 30px 10px",
-            width: "370px",
-            height: "80px",
-            resize: "vertical",
-            border: "1px solid #D9D9D9",
-            padding: "10px",
-            borderRadius: "8px",
-            boxShadow: "0px 3px 5px rgba(0, 0, 0, 0.1)",
-          }}
-        />
+          <textarea
+            placeholder="Enter your message..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            style={{
+              fontSize: 16,
+              margin: "20px 10px 30px 10px",
+              width: "370px",
+              height: "80px",
+              resize: "vertical",
+              border: "1px solid #D9D9D9",
+              padding: "10px",
+              borderRadius: "8px",
+              boxShadow: "0px 3px 5px rgba(0, 0, 0, 0.1)",
+            }}
+          />
 
-        <button style={{ marginLeft: "10px", marginBottom: "10px" }}>
-          Save Update
-        </button>
+          <button style={{ marginLeft: "10px", marginBottom: "10px" }}>
+            Save Update
+          </button>
+        </div>
       </div>
     </div>
   );
